77dcb388c7b20984f2e9280a1f3d46d4499a6ec7
Ayushsingh05 <singhayush28321@gmail.com>
2023-08-21 18:03:26 +0530

    vercel.json added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://physio-assignment.vercel.app/login', // Replace with your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(8081);
 }
 bootstrap();
